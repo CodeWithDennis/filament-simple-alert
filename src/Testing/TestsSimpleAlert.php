30417a3116747356b85fec9f5ebf7a96942288ce
M. D. <dissto@users.noreply.github.com>
2024-10-04 16:29:33 +0200

    Bring back tests

---
+++ src/Testing/TestsSimpleAlert.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CodeWithDennis\SimpleAlert\Testing;
+
+
+use Livewire\Features\SupportTesting\Testable;
+
+/**
+ * @mixin Testable
+ */
+class TestsSimpleAlert{}
---
+++ tests/Pest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace CodeWithDennis\SimpleAlert\Tests;
+
+use CodeWithDennis\SimpleAlert\Tests\TestCase;
+
+uses(TestCase::class)->in(__DIR__);
