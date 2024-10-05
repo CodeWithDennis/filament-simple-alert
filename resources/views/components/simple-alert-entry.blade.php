3f961ba23f3d6b32ae7c6569e4b970d68adaf9df
M. D. <dissto@users.noreply.github.com>
2024-10-06 00:19:13 +0200

    WIP

--- resources/views/components/simple-alert-entry.blade.php
+++ resources/views/components/simple-alert-entry.blade.php
@@ -8,5 +8,7 @@
             :link="$getLink()"
             :link-label="$getLinkLabel()"
             :link-blank="$getLinkBlank()"
+{{--            :actions="$getActions()" https://github.com/CodeWithDennis/filament-simple-alert/pull/20 --}}
+            :actions-vertical-alignment="$getActionsVerticalAlignment()"
     />
 </x-dynamic-component>
--- src/Components/Forms/SimpleAlert.php
+++ src/Components/Forms/SimpleAlert.php
@@ -3,9 +3,11 @@
 namespace CodeWithDennis\SimpleAlert\Components\Forms;
 
 use Closure;
+use CodeWithDennis\SimpleAlert\Components\Concerns\HasActionVerticalAlignment;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasColor;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasDescription;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasIcon;
+use CodeWithDennis\SimpleAlert\Components\Concerns\HasIconVerticalAlignment;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasLink;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasSimple;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasTitle;
@@ -16,11 +18,11 @@ class SimpleAlert extends Field
     use HasColor;
     use HasDescription;
     use HasIcon;
+    use HasIconVerticalAlignment;
     use HasLink;
     use HasSimple;
     use HasTitle;
-
-    protected Closure|string|null $actionsVerticalAlignment = null;
+    use HasActionVerticalAlignment;
 
     protected string $view = 'filament-simple-alert::components.simple-alert-field';
 
@@ -29,18 +31,6 @@ class SimpleAlert extends Field
         $this->actions = $actions;
 
         return $this;
-    }
-
-    public function actionsVerticalAlignment(Closure|string $actionsVerticalAlignment = 'center'): static
-    {
-        $this->actionsVerticalAlignment = $actionsVerticalAlignment;
-
-        return $this;
-    }
-
-    public function getActionsVerticalAlignment(): ?string
-    {
-        return $this->evaluate($this->actionsVerticalAlignment);
     }
 
     protected function setUp(): void
