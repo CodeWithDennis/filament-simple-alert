a3237c2fedaf7090d739d4435cb9fb397ee8142f
Dennis Elsinga <personal.elsinga@gmail.com>
2024-11-12 18:06:54 +0100

    Add HasActionVerticalAlignment and HasIconVerticalAlignment

--- src/Components/Infolists/SimpleAlert.php
+++ src/Components/Infolists/SimpleAlert.php
@@ -3,10 +3,12 @@
 namespace CodeWithDennis\SimpleAlert\Components\Infolists;
 
 use Closure;
+use CodeWithDennis\SimpleAlert\Components\Concerns\HasActionVerticalAlignment;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasBorder;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasColor;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasDescription;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasIcon;
+use CodeWithDennis\SimpleAlert\Components\Concerns\HasIconVerticalAlignment;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasLink;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasSimple;
 use CodeWithDennis\SimpleAlert\Components\Concerns\HasTitle;
@@ -14,10 +16,12 @@ use Filament\Infolists\Components\Entry;
 
 class SimpleAlert extends Entry
 {
+    use HasActionVerticalAlignment;
     use HasBorder;
     use HasColor;
     use HasDescription;
     use HasIcon;
+    use HasIconVerticalAlignment;
     use HasLink;
     use HasSimple;
     use HasTitle;
