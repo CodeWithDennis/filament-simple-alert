44ac20979c83cfc6e7518812a74d7cf57420fc62
CodeWithDennis <personal.elsinga@gmail.com>
2025-06-08 18:51:54 +0200

    Add new icon animations

--- src/Components/Enums/IconAnimation.php
+++ src/Components/Enums/IconAnimation.php
@@ -5,4 +5,6 @@ namespace CodeWithDennis\SimpleAlert\Components\Enums;
 enum IconAnimation: string
 {
     case Spin = 'animate-spin';
+    case Pulse = 'animate-pulse';
+    case Bounce = 'animate-bounce';
 }
